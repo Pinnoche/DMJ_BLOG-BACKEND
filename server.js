c5a14d407cd82de57b826906c7421137ae6defda
Pinnoche <ptime552@gmail.com>
2024-09-11 14:27:08 +0100

    middleware removed

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const app = express()
 //middleware
 app.use(express.json())
 
-app.use((req, res, next) =>{
-    console.log(req.path, req.method)
-    next()
-})
+// app.use((req, res, next) =>{
+//     console.log(req.path, req.method)
+//     next()
+// })
 
 //routing
 app.use('/api/blogs', blogRoutes)
